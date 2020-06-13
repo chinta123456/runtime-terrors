04aa952e1c7b27590e76c8b74e06709813eeef64
MoeJi123 <32314986+MoeJi123@users.noreply.github.com>
2020-06-13 19:22:24 -0400

    Add files via upload
    Moe version

--- app.py
+++ app.py
@@ -1,38 +1,39 @@
-from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
-import scrape_mars
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///city_db")
 
-# Create an instance of Flask
 app = Flask(__name__)
 
-# Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+#################################################
+# Flask Routes
+#################################################
+@app.route("/tour")
+def tour():
+    """List all available api routes."""
+    tour_data = pd.read_sql("SELECT * FROM tour_data",engine).to_json()
+    return (
+        tour_data
+    )
 
+@app.route("/month")
+def month():
+    """List all available api routes."""
+    tour_data = pd.read_sql("SELECT * FROM tour_data",engine).to_json()
+    return (
+        month_data
+    )
 
-# Route to render index.html template using data from Mongo
-@app.route("/")
-def home():
-
-    # Find one record of data from the mongo database
-    mars_data = mongo.db.collection.find_one()
-
-    # Return template and data
-    return render_template("index.html", mars=mars_data)
-
-
-# Route that will trigger the scrape function
-@app.route("/scrape")
-def scrape():
-
-    # Run the scrape function
-    results = scrape_mars.scrape_info()
-
-    # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, results, upsert=True)
-
-    # Redirect back to home page
-    return redirect("/")
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/alchol")
+def month():
+    """List all available api routes."""
+    tour_data = pd.read_sql("SELECT * FROM tour_data",engine).to_json()
+    return (
+        alchol_data
+    )
