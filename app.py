f59b9e95118c82eeef62d991719f8f648fe6e1ba
纪雨萌 <Jiyumeng@jiyumenook10594.fios-router.home>
2020-06-15 22:01:01 -0400

    final flask app - all data is in the api

--- app.py
+++ app.py
@@ -1,38 +1,50 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify, request
 from flask_pymongo import PyMongo
-import scrape_mars
+import os
+
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
-
-
-# Route to render index.html template using data from Mongo
-@app.route("/")
-def home():
-
-    # Find one record of data from the mongo database
-    mars_data = mongo.db.collection.find_one()
-
-    # Return template and data
-    return render_template("index.html", mars=mars_data)
+mongo = PyMongo(app, uri="mongodb://localhost:27017/cityDB")
 
 
 # Route that will trigger the scrape function
-@app.route("/scrape")
-def scrape():
 
-    # Run the scrape function
-    results = scrape_mars.scrape_info()
+@app.route("/citydata", methods=["GET", "POST"])
+def city():
 
-    # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, results, upsert=True)
+    selcity = request.args.get("city")
 
-    # Redirect back to home page
-    return redirect("/")
+    # # bring in the busiest month data
+    query_results = mongo.db.busyMonths_VF.find({selcity: { "$exists": True }})
+    top5 = {"result": []}
+    i = 0
+    for item in query_results:
+        if (i < 5):
+            del item['_id']
+            top5["result"].append(item[selcity])
+            i = i + 1
+ 
+    #bring in the city data
+
+    cursor = mongo.db.tourData.find()
+    
+    results = []
+    for city in cursor:
+
+        if city['selcity'] == selcity:
+        
+            del city['_id']
+            results.append(city)
+
+    datadic = {"top5":top5, "tourdata":results}
+
+    return(datadic)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host=os.getenv('IP', '0.0.0.0'), 
+            port=int(os.getenv('PORT', 4444)),
+            debug=True)
